9594c404eb780034c2836b43fe159b64d5260ce1
upwindgso <tim.jones@upwindgso.com>
2024-12-31 19:24:05 +1100

    finished section 4

--- main.py
+++ main.py
@@ -7,13 +7,19 @@ from langchain.tools.render import render_text_description
 from langchain.prompts import PromptTemplate
 
 from langchain_core.agents import AgentAction, AgentFinish
+
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 
+from callbacks import AgentCallbackHandler
+
+import json
 
 ###load the environment variables
 import os, boilerplate
+
 
 boilerplate.load_dotenv()
 
@@ -28,14 +34,6 @@ boilerplate.load_dotenv()
 # ========
 # Note that we can no longer call it like a regular function and need to use the .invoke method to call it passing the input={dictionary} 
 # ie. print(get_text_length.invoke(input={"text":"Dog"}))  #note the syntax for call it via invoke
-
-@tool 
-def get_text_length(text:str)-> int:
-    """Returns the length of the text in characters.
-    """
-    text = text.strip("'\n").strip('"')
-
-    return len(text)
 
 def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
@@ -54,8 +52,21 @@ def format_log_to_str(
     for action, observation in intermediate_steps:
         thoughts += action.log
         thoughts += f"\n{observation_prefix}{observation}\n{llm_prefix}"
-            
+
+#we do this because the stop token filters out anything from observation onwards...so we nee to build it back in if it exists.
+#kind of hacky / confusing in the flow?
+
     return thoughts
+
+@tool 
+def get_text_length(text:str)-> int:
+    """Returns the length of the text in characters.
+    """
+    text = text.strip("'\n").strip('"')
+
+    return len(text)
+
+
 
         
 
@@ -69,21 +80,22 @@ if __name__ == "__main__":
     Answer the following questions as best you can. You have access to the following tools:
 
     {tools}
-
-    Use the following format:
+    
+    Use the following format in your response:
 
     Question: the input question you must answer
     Thought: you should always think about what to do
+    
     Action: the action to take, should be one of [{tool_names}]
     Action Input: the input to the action
     Observation: the result of the action
     ... (this Thought/Action/Action Input/Observation can repeat N times)
-
-    If you have reached a final answer, respond with:
+    
+    Thought: I now know the final answer
     Final Answer: the final answer to the original input question
 
     Begin!
-
+    
     Question: {input}
     Thought: {agent_scratchpad}
     """
@@ -97,9 +109,14 @@ if __name__ == "__main__":
         tool_names=", ".join([t.name for t in tools])
         )
 
-    #llm = ChatAnthropic(temperature=0.1, model='claude-3-5-haiku-latest', stop=["\nFinal Answer"])
-    llm = ChatOpenAI(temperature=0.0,model="gpt-4o-mini", stop=["\nFinal Answer"])
-    #llm = ChatOllama(temperature=0.1, model="llama3.2_32k", stop=["\nFinal Answer"])
+
+    stop_tokens = [""]#["\nObservation"],
+
+    #llm = ChatAnthropic(temperature=0.0, model='claude-3-5-haiku-latest', stop=stop_tokens, callbacks=[AgentCallbackHandler()])
+    llm = ChatOpenAI(temperature=0.0,model="gpt-4o-mini", stop=["\nObservation"], callbacks=[AgentCallbackHandler()])
+    #llm = ChatOllama(temperature=0.0, model="llama3.2_32k", stop=stop_tokens, callbacks=[AgentCallbackHandler()])
+    #llm = ChatGroq(temperature=0.0, model="llama-3.2-3b-preview", stop=["\nObservation"], callbacks=[AgentCallbackHandler()])
+
 
     intermediate_steps = []
 
@@ -120,23 +137,24 @@ if __name__ == "__main__":
         #| StrOutputParser()       #doesnt show prints inside loop
         )
 
-    isfinished = False
+    agent_step = ""
 
-    while not isfinished:
-
+    while not isinstance(agent_step, AgentFinish):
         ###### Typing
         #this is a way of typing a variable to say that the variable can only contain either an AgentAction or AgentFinish object.
         #the union allows us to join types....so we might say Union[str,int] if we wanted a magic variable that can hold either a string or an integer.
         #syntax: variablename : type1 | type2 | type3 = Value
 
-        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        agent_step: Union[AgentAction, AgentFinish]  = agent.invoke(
             {
-                "input": "What is the length in characters of the text: DOG",
+                "input": "What is the length in characters of the text: DOG?",
                 "agent_scratchpad": intermediate_steps #this is the history of all the steps that have been taken so far.  We pass this in as context to the LLM so it can remember what has happened before.
             }
             )
 
-        #print(agent_step)
+       #output is of type Union[AgentAction, AgentFinish]
+        #we need to check if it is an action or a finish
+
 
         #isinstance checks to see if object a (agent_step) is an instance of the class b (AgentAction).
         #so we receive agent_step....but we dont know if its an action or a finish or a...and isinstance figures out which
@@ -156,22 +174,27 @@ if __name__ == "__main__":
             print(f"{tool_name=}")
             print(f"{tool_input=}")  #huh cool! the fstring {name=} adding = operator prints the variable anme and value...saves redundant boilerplate typing!
             print(f"{observation=}")
-            print(f"{intermediate_steps=}")
+            print("intermediate_steps=" + json.dumps(intermediate_steps, indent=2, default=str,sort_keys=True))
+            
         
-
         elif isinstance(agent_step, AgentFinish):
             #this means that the agent has finished its task and returned a final answer
             print("====================================")
             print(f"{agent_step=}")
-            print(f"{intermediate_steps=}")
-            print("Agent finished with output:", agent_step.return_values["output"])
+            print("intermediate_steps=" + json.dumps(intermediate_steps, indent=2, default=str))
+            print(">>>Agent finished with output:", agent_step.return_values["output"])
             
             #end the loop
             isfinished = True
 
     
 
+"""things to figure out....why is tool_input 'DOG"  \nObservation: 3' rather than just DOG
+    It seems to be related to a bad prompt including additional headings in the input field
 
+    Is the agent expecting some sort of prompt that ive strayed too far from?
+    Now its including everything and its thoughts in the input...
+    """
 
 
     
